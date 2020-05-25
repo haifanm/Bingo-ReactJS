bf1257c57c08d9bac92a0791a8d0645a1128e5b0
haifanm <haifa-naim@hotmail.com>
2020-05-26 02:16:38 +0300

    Updates

---
+++ precache-manifest.f4c3c79b5277be320c2e6e5c8b3acac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3a61783cdb3805dc172cd1a232d447",
+    "url": "/Bingo-ReactJS/index.html"
+  },
+  {
+    "revision": "0fd3e5711ad743229c34",
+    "url": "/Bingo-ReactJS/static/css/main.88bdb4c6.chunk.css"
+  },
+  {
+    "revision": "10fd27bc4a2a7dcfd0a3",
+    "url": "/Bingo-ReactJS/static/js/2.1b470128.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/Bingo-ReactJS/static/js/2.1b470128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd3e5711ad743229c34",
+    "url": "/Bingo-ReactJS/static/js/main.90134a6d.chunk.js"
+  },
+  {
+    "revision": "eee10a5567583122015c",
+    "url": "/Bingo-ReactJS/static/js/runtime-main.fac9da84.js"
+  }
+]);
